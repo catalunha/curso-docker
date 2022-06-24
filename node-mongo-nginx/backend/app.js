d199a96b3ce19e38db0f0f8625ce78b2d18db257
catalunha <catalunha.mj@gmail.com>
2022-06-23 22:13:56 -0300

    compose ainda com erro

--- node-mongo-nginx/backend/app.js
+++ node-mongo-nginx/backend/app.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const restful = require("node-restful");
-const server = express();
-const mongoose = restful.mongoose;
+const express = require('express')
+const restful = require('node-restful')
+const server = express()
+const mongoose = restful.mongoose
 
-//Database
-mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://db/mydb");
+// Database
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://db/mydb')
 
 //teste
 server.get('/',(req,res,next)=>res.send('Backend'));
